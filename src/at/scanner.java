a087cf0e34b0bd9ee595b4fb3089298647435bc8
Julia Winder <julia.winder@student.htldornbirn.at>
2023-11-16 15:45:56 +0100

    hallo read me

--- src/at/scanner.java
+++ src/at/scanner.java
@@ -3,16 +3,51 @@ package at;
 import java.util.Scanner;
 
 public class scanner {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Scanner scanner = new Scanner(System.in);
+        double kontostand =0.0;
         int auswahl =0;
+        int input =0;
         int i = 0;
-        scanner.nextInt();
-
+       
         do{
+            System.out.println("1. einzahlen");
+            System.out.println("2. Abheben");
+            System.out.println("3. Kontostand");
+            System.out.println("4. Beenden");
             System.out.print("Bitte den Wert eingeben:");
             auswahl = scanner.nextInt();
             System.out.println("Hier kommt der Wert:"+ auswahl);
-        }while ();
+        }while (auswahl != 4);
+
+        int option = input;
+
+        switch (option) {
+            case 1:
+                System.out.print("Geben Sie den Einzahlungsbetrag ein: ");
+                double auswahl = scanner.nextDouble();
+            {double kontostand = auswahl;
+                System.out.println("Einzahlung erfolgreich!");}
+                break;
+            case 2:
+                System.out.print("Geben Sie den Abhebungsbetrag ein: ");
+                double auswahl = input;
+                if (auswahl > kontostand) {
+                    System.out.println("Nicht genügend Guthaben auf dem Konto.");
+                } else {
+                    kontostand -= auswahl;
+                    System.out.println("Auszahlung erfolgreich!");
+                }
+                break;
+            case 3:
+                System.out.println("Ihr Kontostand beträgt: " + kontostand);
+                break;
+            case 4:
+                System.out.println("Vielen Dank und auf Wiedersehen!");
+                System.exit(0);
+            default:
+                System.out.println("Ungültige Option. Bitte wählen Sie erneut.");
+        }
     }
 }
